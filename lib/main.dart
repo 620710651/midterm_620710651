7f6ceb9c0ca2caa0d6ff7ad2179f80c2c44dab19
Jidapa Gondin <GONDIN_J@silpakorn.edu>
2023-01-15 11:58:39 +0700

    midterm

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:guess_number/pages/game/game_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Midterm());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class Midterm extends StatelessWidget {
+  const Midterm({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'midterm',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: GamePage(),
+      home: const MyHomePage(title: 'Calendar 2023'),
     );
   }
 }
+  class MyHomePage extends StatefulWidget {
+   const MyHomePage({super.key, required this.title});
+   final String title;
+    @override
+   State<MyHomePage> createState() => _MyHomePageState();
+}
+  class _MyHomePageState extends State<MyHomePage> {
+   void _incrementCounter() {
+     setState(() {});
+  }
 
+  @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: OutlinedButton(
+                        onPressed: () {
+                          //todo:
+                        },
+                        child: Text('January'),
+
+                      ),
+                    ),
+                  ],
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
